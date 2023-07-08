2114cc5abe294611d6c46dc2f1adf6e13c41c1b8
Anastasya_Tolstsikava <Anastasya_Tolstsikava@epam.com>
2023-07-08 16:14:22 +0300

    Delete unused logic, code refactoring

--- src/main/java/com/tolstsikava/testproject/demo/SalaryCalculationServiceApplication.java
+++ src/main/java/com/tolstsikava/testproject/demo/SalaryCalculationServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SalaryCalculationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalaryCalculationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryCalculationServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/tolstsikava/testproject/demo/employee/EmployeeWorkingHours.java
+++ src/main/java/com/tolstsikava/testproject/demo/employee/EmployeeWorkingHours.java
@@ -1,10 +1,8 @@
 package com.tolstsikava.testproject.demo.employee;
 
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record EmployeeWorkingHours(UUID id, LocalDateTime startTime, LocalDateTime endTime, int loggedTimeInMinutes) {
-//    public static final EmployeeWorkingHours DEFAULT = new EmployeeWorkingHours(, null, null, 0);
+
 }
